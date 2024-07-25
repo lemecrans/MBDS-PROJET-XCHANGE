d25eaecc71c281a134243b2c702cf9b175251efb
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2024-07-25 20:59:04 +0300

    Ecran d'introduction
    - Ajout de l'écran d'introduction
    - Import des ressources nécessaires

---
+++ Android/app/src/main/java/com/mbds/tpt_android/Activities/Intro_activity.java
@@ -0,0 +1,16 @@
+package com.mbds.tpt_android.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mbds.tpt_android.R;
+
+public class Intro_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
